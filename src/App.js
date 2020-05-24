1bb5f7d569bb3dd0315066f6dba7ead0891b1364
kingsley okpara <kingsconsult001@gmail.com>
2020-05-24 08:25:14 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const message = "This is my first variable rendered in JSX!";
+
+  const handleClick = () => {
+    alert("you clicked the message!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello kings</h1>
+        <h2 onClick={handleClick}>{message}</h2>
+
       </header>
     </div>
   );
 }
 
 export default App;
+
